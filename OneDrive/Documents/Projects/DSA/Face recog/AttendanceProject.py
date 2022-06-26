5db7377e57176a9b5e832fc0dcbd611ea78bae11
Hrithik <hrithikumar03@gmail.com>
2022-06-26 18:37:39 +0530

    Basic face recognition software using opencv

---
+++ OneDrive/Documents/Projects/DSA/Face recog/AttendanceProject.py
@@ -0,0 +1,80 @@
+import cv2
+import numpy as np
+import face_recognition
+import os
+#import pyttsx3
+
+#engine = pyttsx3.init()
+#engine.setProperty('rate',120)
+#voices = engine.getProperty('voices')
+#engine.setProperty('voice',voices[1].id)
+
+path = 'C:/Users/Hrithik/OneDrive/Documents/Projects/DSA/Face recog/Attendance_images'
+images = []
+classnames = []
+
+
+
+myList = os.listdir(path)
+print(myList)
+
+for i in myList:
+    cur = cv2.imread(f'{path}/{i}')
+    images.append(cur)
+    classnames.append(os.path.splitext(i)[0])
+
+print(classnames)
+
+def find_encodings(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+
+    return encodeList
+
+
+encodings = find_encodings(images)
+
+print("Encoding Complete...")
+
+capture = cv2.VideoCapture(0)
+
+while True:
+    success, img = capture.read()
+    imgS = cv2.resize(img, (0,0), None,0.25,0.25)
+    imgS = cv2.cvtColor(imgS,cv2.COLOR_BGR2RGB)
+
+
+    faces = face_recognition.face_locations(imgS)
+    encode = face_recognition.face_encodings(imgS, faces) 
+
+    for encodeface, faceloc in zip(encode,faces):
+        matching = face_recognition.compare_faces(encodings, encodeface)
+        facedist = face_recognition.face_distance(encodings, encodeface)
+        index = np.argmin(facedist)
+        #print(max(facedist),min(facedist),max(facedist)-min(facedist))
+
+        if matching[index] and ((max(facedist)-min(facedist))>0.14):
+            name = classnames[index].upper()
+            #print(name)
+            y1,x2,y2,x1 = faceloc
+            y1,x2,y2,x1 = y1*4,x2*4,y2*4,x1*4
+            cv2.rectangle(img,(x1,y1),(x2,y2),(255,0,0),1)
+            cv2.rectangle(img,(x1,y2-35),(x2,y2),(255,0,0),cv2.FILLED)
+            cv2.putText(img, name,(x1+6,y2-6),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255),1)
+
+
+                
+        else:
+            y1,x2,y2,x1 = faceloc
+            y1,x2,y2,x1 = y1*4,x2*4,y2*4,x1*4
+            cv2.rectangle(img,(x1,y1),(x2,y2),(255,0,0),1)
+            cv2.rectangle(img,(x1,y2-35),(x2,y2),(255,0,0),cv2.FILLED)
+            cv2.putText(img, "UNKNOWN",(x1+6,y2-6),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255),1)
+
+    cv2.imshow('Webcam', img)
+    cv2.waitKey(1)
+
+
---
+++ OneDrive/Documents/Projects/DSA/Face recog/basic.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy as np
+import face_recognition
+
+#recognising face in the image and convertion color from BGR to RGB
+vijayimg = face_recognition.load_image_file("C:/Users/Hrithik/OneDrive/Documents/Projects/DSA/Face recog/images/vijay.jpg")
+vijayimg = cv2.cvtColor(vijayimg,cv2.COLOR_BGR2RGB)
+
+#recognising face in the test image and convertion color from BGR to RGB
+testimg = face_recognition.load_image_file('C:/Users/Hrithik/OneDrive/Documents/Projects/DSA/Face recog/images/surya.jpg')
+testimg = cv2.cvtColor(testimg,cv2.COLOR_BGR2RGB)
+
+#getting the face locations in the image and test image and making a rectangle around the located faces 
+face_loc = face_recognition.face_locations(vijayimg)[0]
+encodevijay = face_recognition.face_encodings(vijayimg)[0]
+cv2.rectangle(vijayimg,(face_loc[3],face_loc[0]),(face_loc[1],face_loc[2]),(0,255,0),2)
+
+face_loc_test = face_recognition.face_locations(testimg)[0]
+encodetest = face_recognition.face_encodings(testimg)[0]
+cv2.rectangle(testimg,(face_loc_test[3],face_loc_test[0]),(face_loc_test[1],face_loc_test[2]),(0,255,0),2)
+
+result = face_recognition.compare_faces([encodevijay],encodetest)
+face_dist = face_recognition.face_distance([encodevijay],encodetest)
+print(result)
+print(face_dist)
+
+cv2.putText(testimg,f'{result}{round(face_dist[0],2)}',(10,20),cv2.FONT_HERSHEY_COMPLEX_SMALL,1,(0,255,0),1)
+
+cv2.imshow('vijay',vijayimg)
+cv2.imshow('vijay-test',testimg)
+cv2.waitKey(0)
+
